2f60737eac1786ca1c751b5386af2f74b8f052ae
Itiu <ValeriyBushenev@gmail.com>
2010-10-12 01:30:04 +0400

    +1

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 rm *.o
 rm *.a
+git log -1 --pretty=format:"module myversion; public final static char[] author=\"%an\"; public final static char[] hash=\"%h\";">myversion.d
 dmd src/mongo.d src/bson.d src/md5.d -Hdexport -release -lib -oflibmongod
